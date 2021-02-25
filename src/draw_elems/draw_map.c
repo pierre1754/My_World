e3d7097833e57751338b6131047ddde80233c950
Pierre <pierre.mauger@epitech.eu>
2021-02-25 11:44:59 +0100

    create map correctly and get vertex

--- src/draw_elems/draw_map.c
+++ src/draw_elems/draw_map.c
@@ -7,23 +7,6 @@
 
 #include "my_world.h"
 
-sfVertexArray *create_vertex_quad(sfVector2f *point1, sfVector2f *point2,
-sfVector2f *point3, sfVector2f *point4)
-{
-    sfVertexArray *array;
-    sfVertex vertex1 = {.position = *point1, .color = sfGreen};
-    sfVertex vertex2 = {.position = *point2, .color = sfYellow};
-    sfVertex vertex3 = {.position = *point3, .color = sfWhite};
-    sfVertex vertex4 = {.position = *point4, .color = sfYellow};
-
-    sfVertexArray_append(array, vertex1);
-    sfVertexArray_append(array, vertex2);
-    sfVertexArray_append(array, vertex3);
-    sfVertexArray_append(array, vertex4);
-    sfVertexArray_setPrimitiveType(array, sfQuads);
-    return array;
-}
-
 void draw_2d_map(void)
 {
     engine_t *engine = get_engine();
