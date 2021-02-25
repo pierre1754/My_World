6c9a4c8c36f2e8f2a9b345ed5d3e6ded03d901ef
pierre1754 <pierre1754@outlook.com>
2021-02-25 16:43:34 +0100

    fix problems

--- src/init_elems/init_elem.c
+++ src/init_elems/init_elem.c
@@ -9,13 +9,7 @@
 
 void init_elem(void)
 {
-<<<<<<< HEAD
     init_map();
-=======
-    init_map_3d();
-    init_map_2d();
-    init_map_ver();
     init_button();
->>>>>>> 8ea559f0b8c4043806de150beff6c8d81d5b8720
     // PUT INIT HERE
 }
