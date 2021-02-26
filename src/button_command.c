b24a62362a03d9c05f53f90b5638e37215fea812
Pierre <pierre.mauger@epitech.eu>
2021-02-26 11:14:29 +0100

    create button functions

--- src/button_command.c
+++ src/button_command.c
@@ -7,18 +7,52 @@
 
 #include "my_world.h"
 
-void enlarge_map_x(void)
+void resize_map(void)
+{
+    engine_t *engine = get_engine();
+
+    GET_MAP_3D(engine) = intdup_2d(GET_MAP_3D(engine));
+    GET_MAP_2D(engine) = vectordup_2d(GET_MAP_2D(engine));
+    GET_MAP_VER(engine) = vertexdup_2d(GET_MAP_VER(engine));
+    init_map_2d();
+    init_map_ver();
+}
+
+void increase_map_x(void)
 {
     engine_t *engine = get_engine();
 
     if (GET_SET_MX(engine) > 0) {
         GET_SET_MX(engine) += 1;
-        GET_MAP_3D(engine) = intdup_2d(GET_MAP_3D(engine));
-        GET_MAP_2D(engine) = vectordup_2d(GET_MAP_2D(engine));
-        GET_MAP_VER(engine) = vertexdup_2d(GET_MAP_VER(engine));
-        //GET_SET_MY(engine) -= 1;
-        //init_map_3d();
-        init_map_2d();
-        init_map_ver();
+        resize_map();
+    }
+}
+void decrease_map_x(void)
+{
+    engine_t *engine = get_engine();
+
+    if (GET_SET_MX(engine) > 0) {
+        GET_SET_MX(engine) -= 1;
+        resize_map();
+    }
+}
+
+void increase_map_y(void)
+{
+    engine_t *engine = get_engine();
+
+    if (GET_SET_MY(engine) > 0) {
+        GET_SET_MY(engine) += 1;
+        resize_map();
+    }
+}
+
+void decrease_map_y(void)
+{
+    engine_t *engine = get_engine();
+
+    if (GET_SET_MY(engine) > 0) {
+        GET_SET_MY(engine) -= 1;
+        resize_map();
     }
 }
