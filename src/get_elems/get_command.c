2516e6a1fe8818c6d69fd9d0800f9eb08f47326a
Pierre <pierre1754@outlook.com>
2021-03-04 15:41:39 +0100

    key changed

--- src/get_elems/get_command.c
+++ src/get_elems/get_command.c
@@ -12,20 +12,8 @@ void move_command(void)
     engine_t *engine = get_engine();
 
     if (engine->event.type == sfEvtKeyPressed) {
-<<<<<<< HEAD
         key_move();
         key_zoom();
         resize_map();
-=======
-        if (engine->event.key.code == sfKeyRight)
-            GET_SET_PX(engine) += 20;
-        if (engine->event.key.code == sfKeyLeft)
-            GET_SET_PX(engine) -= 20;
-        if (engine->event.key.code == sfKeyDown)
-            GET_SET_PY(engine) += 20;
-        if (engine->event.key.code == sfKeyUp)
-            GET_SET_PY(engine) -= 20;
-        recalc_map();
->>>>>>> 1dccedffc52c12ca379453fdc1114c2897aa82c7
     }
 }
