bd9c696b7bfe4eace2a63f1fc72eb35b83518d34
Pierre <pierre.mauger@epitech.eu>
2021-03-04 15:15:51 +0100

    start working on free

--- src/button_command/change_angle.c
+++ src/button_command/change_angle.c
@@ -7,12 +7,25 @@
 
 #include "my_world.h"
 
+void change_angle(void)
+{
+    engine_t *engine = get_engine();
+
+    //clear_map_ver(GET_MAP_VER(engine));
+    init_map_2d();
+    init_map_2d_base();
+    init_map_ver(GET_MAP_VER(engine));
+    init_map_ver_base(GET_MAP_ORIGIN(engine));
+    clear_map_ver(GET_MAP_LINES(engine));
+    init_map_line();
+}
+
 void increase_angle_x(void)
 {
     engine_t *engine = get_engine();
 
     GET_SET_AX(engine)++;
-    resize_map();
+    change_angle();
 }
 
 void decrease_angle_x(void)
@@ -20,7 +33,7 @@ void decrease_angle_x(void)
     engine_t *engine = get_engine();
 
     GET_SET_AX(engine)--;
-    resize_map();
+    change_angle();
 }
 
 void increase_angle_y(void)
@@ -28,7 +41,7 @@ void increase_angle_y(void)
     engine_t *engine = get_engine();
 
     GET_SET_AY(engine)++;
-    resize_map();
+    change_angle();
 }
 
 void decrease_angle_y(void)
@@ -36,5 +49,5 @@ void decrease_angle_y(void)
     engine_t *engine = get_engine();
 
     GET_SET_AY(engine)--;
-    resize_map();
+    change_angle();
 }
