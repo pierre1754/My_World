eafcfe30dd8c05c85cb6d0e284a59fbd8b12bfc3
Pierre <pierre.mauger@epitech.eu>
2021-03-15 10:09:14 +0100

    fix water rendering on height 0

--- src/draw_elems/draw_shader.c
+++ src/draw_elems/draw_shader.c
@@ -11,8 +11,9 @@ void draw_shader(int i, int j)
 {
     engine_t *engine = get_engine();
 
-    if (i + 2 != GET_SET_MX(engine) && j + 2 != GET_SET_MY(engine) &&
-    GET_MAP_3D(engine)[i][j] < 0)
+    if (i + 1 != GET_SET_MX(engine) && j + 1 != GET_SET_MY(engine) &&
+    GET_MAP_3D(engine)[i][j] <= 0 && (GET_MAP_3D(engine)[i + 1][j] < 0 ||
+    GET_MAP_3D(engine)[i][j + 1] < 0))
         sfRenderWindow_drawVertexArray(GET_WINDOW(engine),
         GET_MAP_ORIGIN(engine)[i][j], &engine->shade->states);
 }
