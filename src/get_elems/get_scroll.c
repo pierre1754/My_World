6993fe8aaa51ba011a104fe6623c635471791601
Pierre <pierre.mauger@epitech.eu>
2021-03-18 11:57:09 +0100

    fix 2 conditionnal branching

--- src/get_elems/get_scroll.c
+++ src/get_elems/get_scroll.c
@@ -7,6 +7,16 @@
 
 #include "my_world.h"
 
+static void get_scroll_minus(void)
+{
+    engine_t *engine = get_engine();
+
+    if (engine->event.type == sfEvtMouseWheelScrolled &&
+    engine->event.mouseWheelScroll.delta > 0)
+        if (GET_SET_RAD(engine) - 10 > 0)
+            GET_SET_RAD(engine) -= 10;
+}
+
 void get_scroll(void)
 {
     engine_t *engine = get_engine();
@@ -14,8 +24,6 @@ void get_scroll(void)
     if (engine->event.type == sfEvtMouseWheelScrolled &&
     engine->event.mouseWheelScroll.delta < 0)
         GET_SET_RAD(engine) += 10;
-    else if (engine->event.type == sfEvtMouseWheelScrolled &&
-    engine->event.mouseWheelScroll.delta > 0)
-        if (GET_SET_RAD(engine) - 10 > 0)
-            GET_SET_RAD(engine) -= 10;
+    else
+        get_scroll_minus();
 }
--- src/set_elems/set_texture_params.c
+++ src/set_elems/set_texture_params.c
@@ -7,17 +7,22 @@
 
 #include "my_world.h"
 
-void set_texture_params(int i, int j)
+static void set_texture(int i, int j)
 {
     engine_t *engine = get_engine();
 
-    if (i != 0)
-        if (GET_MAP_3D(engine)[i][j + 1] > GET_MAP_3D(engine)[i + 1][j] ||
-        GET_MAP_3D(engine)[i][j + 1] > GET_MAP_3D(engine)[i][j] ||
-        GET_MAP_3D(engine)[i][j] > GET_MAP_3D(engine)[i + 1][j]) {
-            for (int a = 0; a < 4; a++) {
-                sfVertexArray_getVertex(GET_MAP_TEX(engine)[i][j], a)->color =
-                sfColor_fromRGBA(200, 200, 200, 255);
-            }
+    if (GET_MAP_3D(engine)[i][j + 1] > GET_MAP_3D(engine)[i + 1][j] ||
+    GET_MAP_3D(engine)[i][j + 1] > GET_MAP_3D(engine)[i][j] ||
+    GET_MAP_3D(engine)[i][j] > GET_MAP_3D(engine)[i + 1][j]) {
+        for (int a = 0; a < 4; a++) {
+            sfVertexArray_getVertex(GET_MAP_TEX(engine)[i][j], a)->color =
+            sfColor_fromRGBA(200, 200, 200, 255);
         }
+    }
+}
+
+void set_texture_params(int i, int j)
+{
+    if (i != 0)
+        set_texture(i, j);
 }
