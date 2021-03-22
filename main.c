e3e2942c29802b415d0cfba39f2d4208b797ad7f
Pierre <pierre1754@outlook.com>
2021-03-22 23:38:34 +0100

    norme main

--- main.c
+++ main.c
@@ -7,23 +7,26 @@
 
 #include "my_world.h"
 
-static int parcing(int argc, char **argv, char **file)
+static int error_hand(int argc, char **argv, char **file)
 {
-    if (argc == 3 && !my_strcmp(argv[2], "--image")) {
-        if (loop_image(argv[1]))
-            return 84;
-        return 0;
-    }
-    if (argc > 2)
+    if (argc > 2 || !sfShader_isAvailable())
         return 84;
     else if (argc == 2) {
         *file = read_map(argv[1]);
         if (!*file)
             return 84;
     }
-    if (!sfShader_isAvailable())
-        return 84;
     return -1;
+}
+
+static int parcing(int argc, char **argv, char **file)
+{
+    if (argc == 3 && !my_strcmp(argv[2], "--image")) {
+        if (loop_image(argv[1]))
+            return 84;
+        else return 0;
+    }
+    return error_hand(argc, argv, file);
 }
 
 int main(int argc, char **argv)
